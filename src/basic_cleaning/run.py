7aebbebe01409202c853fb0ed0925f1aa6cadff0
ata1314 <ata1314@wgu.edu>
2026-02-01 09:34:50 -0600

    Add geographic boundary filter to basic_cleaning for new data

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -11,6 +11,7 @@ import pandas as pd
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
+
 # DO NOT MODIFY
 def go(args):
 
@@ -18,85 +19,80 @@ def go(args):
     run.config.update(args)
 
     # Download input artifact. This will also log that this script is using this
-    
-    run = wandb.init(project="nyc_airbnb", group="cleaning", save_code=True)
     artifact_local_path = run.use_artifact(args.input_artifact).file()
     df = pd.read_csv(artifact_local_path)
+
     # Drop outliers
     min_price = args.min_price
     max_price = args.max_price
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
+
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
-    # Step 6: TODO
-    # Only implement this step when reaching Step 6: Pipeline Release and Updates
-    # in the project.
-    # Add longitude and latitude filter to allow test_proper_boundaries to pass
-    # ENTER CODE HERE
+    # Step 6: Filter geographic outliers
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.50, 41.20)
+    df = df[idx].copy()
 
     # Save the cleaned data
-    df.to_csv('clean_sample.csv',index=False)
+    df.to_csv('clean_sample.csv', index=False)
 
     # log the new data.
     artifact = wandb.Artifact(
-     args.output_artifact,
-     type=args.output_type,
-     description=args.output_description,
- )
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
     artifact.add_file("clean_sample.csv")
     run.log_artifact(artifact)
 
 
-# TODO: In the code below, fill in the data type for each argument. The data type should be str, float or int. 
-# TODO: In the code below, fill in a description for each argument. The description should be a string.
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="A very basic data cleaning")
-  
+
     parser.add_argument(
-        "--input_artifact", 
-        type = str,
-        help = "Input artifact name",
-        required = True
+        "--input_artifact",
+        type=str,
+        help="Input artifact name",
+        required=True
     )
 
     parser.add_argument(
-        "--output_artifact", 
-        type = str,
-        help = "Output artifact name",
-        required = True
+        "--output_artifact",
+        type=str,
+        help="Output artifact name",
+        required=True
     )
 
     parser.add_argument(
-        "--output_type", 
-        type = str,
-        help = "Output artifact type",
-        required = True
+        "--output_type",
+        type=str,
+        help="Output artifact type",
+        required=True
     )
 
     parser.add_argument(
-        "--output_description", 
-        type = str,
-        help = "Description of output artifact",
-        required = True
+        "--output_description",
+        type=str,
+        help="Description of output artifact",
+        required=True
     )
 
     parser.add_argument(
-        "--min_price", 
-        type = float,
-        help = "Minimum price threshold",
-        required = True
+        "--min_price",
+        type=float,
+        help="Minimum price threshold",
+        required=True
     )
 
     parser.add_argument(
         "--max_price",
-        type = float,
-        help = "Maximum price threshold",
-        required = True
+        type=float,
+        help="Maximum price threshold",
+        required=True
     )
-
 
     args = parser.parse_args()
 
